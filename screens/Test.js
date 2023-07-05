5c5852d35d947ce1ec8305e9fff36f51640975b6
Neoself <neoself1105@gmail.com>
2023-07-05 15:37:52 +0900

    AuthTest

--- screens/Test.js
+++ screens/Test.js
@@ -8,11 +8,13 @@ import { Button, ButtonText } from '../components/CreateRoute_Shared';
 WebBrowser.maybeCompleteAuthSession();
 
 const Test = () => {
+  const androidClientId = '960989891259-cd3e0f1r6e41bu4la3t8h3ufrp4iqgo8.apps.googleusercontent.com';
+  const webClientId = '336901540265-8up6724ndjo4aif20k60ua8cs2f94rnd.apps.googleusercontent.com';
   const [accessToken, setAccessToken] = useState(null);
   const redirectUri = AuthSession.makeRedirectUri({ useProxy: true });
   const handleLogin = async () => {
     const response = await AuthSession.startAsync({
-      authUrl: `https://accounts.google.com/o/oauth2/v2/auth?client_id=336901540265-8up6724ndjo4aif20k60ua8cs2f94rnd.apps.googleusercontent.com&redirect_uri=${encodeURIComponent(
+      authUrl: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${androidClientId}&redirect_uri=${encodeURIComponent(
         redirectUri,
       )}&response_type=code&scope=email%20profile%20openid`,
     });
