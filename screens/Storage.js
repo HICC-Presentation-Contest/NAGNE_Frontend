047585f7bedcc73300a3c82a9c59818cd5998033
Choi Seho <kindseho@naver.com>
2023-07-05 18:26:03 +0900

    feat: 사용자마다 헤더 변경 및 뒤로가기 구현

--- screens/Storage.js
+++ screens/Storage.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import Maps from '../components/Maps';
 import { FlatList } from 'react-native-gesture-handler';
 import { Image } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Container = styled.View`
   flex: 1;
@@ -34,25 +35,28 @@ export default function Storage() {
   const [post, setPost] = useState();
 
   let JWTToken =
-    'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2ODg0NTc2NDEsImV4cCI6MTY4OTA2MjQ0MSwic3ViIjoic2Vobzc4QGcuaG9uZ2lrLmFjLmtyIiwiVE9LRU5fVFlQRSI6IkFDQ0VTU19UT0tFTiJ9.XRGs2XPdCrHpPgkcd96O46r2l8KRYpkZQu8PRfZoDtxYPV3N4-l5QB7IL77BfJMS-9Y7HyZssjPoxDYXAZVrrA';
+    'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2ODg1NDY2NTIsImV4cCI6MTY4OTE1MTQ1Miwic3ViIjoic2Vobzc4QGcuaG9uZ2lrLmFjLmtyIiwiVE9LRU5fVFlQRSI6IkFDQ0VTU19UT0tFTiJ9.P81MwwK7CR5kyTa--S7KX5zqRPM3mWzGg_JQoi7dgWIBn5RtbXABde4MXizmY7lXkpOU6fvmKQFwpxot48kQog';
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`http://3.37.189.80/bookmark?page=0&size=10`, {
-          headers: { Authorization: `Bearer ${JWTToken}` },
-        });
-        console.log('성공'); // Server response data
-        console.log(response.data); // Server response data
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`http://3.37.189.80/bookmark?page=0&size=10`, {
+            headers: { Authorization: `Bearer ${JWTToken}` },
+          });
+          console.log('성공'); // Server response data
+          console.log(response.data); // Server response data
 
-        setPost(response.data.content);
-        // Perform necessary actions with the response data
-      } catch (error) {
-        console.error(error); // Error handling
-      }
-    };
-    fetchData();
-  }, []);
+          setPost(response.data.content);
+          // Perform necessary actions with the response data
+        } catch (error) {
+          console.error(error); // Error handling
+        }
+      };
+
+      fetchData();
+    }, []),
+  );
 
   const renderItem = ({ item: post }) => {
     return (
